04b5aef8e0a7cd6425cc8f9b5ef4a493b3795ad3
k.taniuchi <barbeekonta1995@gmail.com>
2021-12-11 23:16:20 +0900

    fetchAPIを追加

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+document.getElementById('quiz-start-btn').addEventListener('click', () => {
+    fetch("https://opentdb.com/api.php?amount=10")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+});
