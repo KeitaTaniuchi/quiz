20cd63fd5ca75a4e7c164078747b00f5bc6029a9
k.taniuchi <barbeekonta1995@gmail.com>
2021-12-11 23:18:18 +0900

    非同期処理をasync/awaitに変更

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,14 +1,30 @@
 "use strict";
 
+const h1 = document.getElementsByTagName('h1');
+
 document.getElementById('quiz-start-btn').addEventListener('click', () => {
-    fetch("https://opentdb.com/api.php?amount=10")
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.log(error);
-    })
+    fetchQuizJsonData();
 });
+
+//webAPIからJSONデータを作成する関数
+const fetchQuizJsonData = async () => {
+    const response = await fetch("https://opentdb.com/api.php?amount=10");
+    console.log(response)
+    const quizArrData = await response.json();
+    console.log(quizArrData)
+    const A = quizArrData.results;
+    console.log(A)
+    createQuizList(A);
+}
+
+const createQuizList = (A) => {
+  
+    
+
+    /* object.keys(quizJsonData).forEach(key => {
+        console.log(key + ": " + jsonData[key]);
+    }) */
+    
+    const value = A[0].category;
+    console.log(value)
+}
