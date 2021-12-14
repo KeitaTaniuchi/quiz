4041592a061e707c1025d0ba2aafe07704fc0585
k.taniuchi <barbeekonta1995@gmail.com>
2021-12-14 22:07:21 +0900

    クイズのカテゴリーと難易度を記述するpタグを追加、多次元配列の値を取得する処理を追加

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,30 +1,49 @@
 "use strict";
 
-const h1 = document.getElementsByTagName('h1');
+const h1 = document.getElementById('h1');
+const p = document.getElementById('p');
+const quizStartBtn = document.getElementById('quiz-start-btn')
 
-document.getElementById('quiz-start-btn').addEventListener('click', () => {
-    fetchQuizJsonData();
-});
-
-//webAPIからJSONデータを作成する関数
-const fetchQuizJsonData = async () => {
-    const response = await fetch("https://opentdb.com/api.php?amount=10");
-    console.log(response)
-    const quizArrData = await response.json();
-    console.log(quizArrData)
-    const A = quizArrData.results;
-    console.log(A)
-    createQuizList(A);
+class quizList {
+    constructor()
 }
 
-const createQuizList = (A) => {
-  
-    
+//開始ボタンを押した時の処理
+quizStartBtn.addEventListener('click', () => {
+    quizStartBtn.style.display = 'none';
+    fetchQuizData();
+});
 
-    /* object.keys(quizJsonData).forEach(key => {
-        console.log(key + ": " + jsonData[key]);
-    }) */
-    
-    const value = A[0].category;
-    console.log(value)
+//webAPIから取ってきたクイズデータを、多次元配列に変換する処理
+const fetchQuizData = async () => {
+    const response = await fetch("https://opentdb.com/api.php?amount=10");
+    displayInAcquisition();
+
+    const quizData = await response.json();
+
+    const quizDataArr = await quizData.results;
+    createQuiz(quizDataArr);
+}
+
+//クイズデータ作成中の画面表示を行う処理
+const displayInAcquisition = () => {
+    h1.innerHTML = "取得中";
+    p.innerHTML = "少々お待ちください"
+}
+
+const createQuiz = (quizDataArr) => {
+    const quizAnswersContainer = document.getElementById('quiz-answers-container');
+    const createUl = document.createElement('ul');
+    const createLi = document.createElement('li');
+
+    Object.keys(quizDataArr).forEach((currentValue) => {
+        const quizNumber = currentValue;
+        const quizElem = quizDataArr[currentValue];
+        console.log(quizNumber);
+        console.log(quizElem);
+
+        quizAnswersContainer.appendChild(createUl);
+        quizAnswersContainer.appendChild(createLi);
+    });
+
 }
